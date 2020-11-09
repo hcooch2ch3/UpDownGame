befc7e6d8c64d6c67d32ff88f1023c6589fe5765
SM <hcooch2ch3@gmail.com>
2020-11-09 23:03:33 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#UpDownGame for iOS
+
+이 앱은 야곰닷넷의 '왕초보를 위한 iOS 앱개발' 강좌를 수강하면서 만든 앱입니다.
+
+0~30 중에 임의의 값을 5번의 기회 안에 맞추는 게임입니다.
+
+Link: [야곰닷넷](https://yagom.net/courses/ios-starter-uikit/)
---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,16 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 임성민 on 2020/11/02.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
